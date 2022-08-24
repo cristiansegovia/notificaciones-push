5a75543d3873f1259b6da36a31b71064eba5fad1
cristiansegovia <cristianxsegovia@gmail.com>
2022-08-24 17:39:15 -0300

    actualizar mongoose-morgan para error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
-const morgan = require('morgan')
+const morgan = require('mongoose-morgan')
 const path = require("path")
 const app = express()
 
